a750d0e78a47d879924f4ede0bc755dc0f3fc1c0
erenchaglarr <s245735@dtu.dk>
2026-01-06 16:14:37 +0100

    Changes

--- model.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-
-class MyAwesomeModel(nn.Module):
-    """My awesome model."""
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.fc1 = nn.Linear(784, 128)
